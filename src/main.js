180a23c77a224cd2c3f83bc0c9eca8801c78b091
martinyounghoonkim <hoons0131@gmail.com>
2018-04-14 20:39:57 +0900

    Move firebase config files

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from './router'
 import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.use(require('vue-moment'))
+Vue.use(require('vue-moment'));
 
-var bus = new Vue()
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
